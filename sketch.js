3f3a7cea4f95b083c96c07089f05ff46b90f24d0
MichelLap <48298196+MichelLap@users.noreply.github.com>
2021-05-02 17:25:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(401, 401);
 }
-
+let x=0;
 function draw() {
-  background(220);
-  ellipse(50,50,80,80);
+  if (x<401) {
+    background(255);
+    circle(200, 200, x);
+    x += 1;
+  } 
 }
