33ad335caaf9b04e4c383611dbefbd2d794e5efa
MichelLap <48298196+MichelLap@users.noreply.github.com>
2021-05-03 18:51:12 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ let x = 0;
 
 function setup() {
   background(100);  
+  drawingContext.shadowOffsetX = 5;
+  drawingContext.shadowOffsetY = -5;
+  drawingContext.shadowBlur = 10;
+  drawingContext.shadowColor = "black";
 }
 
 function draw() {
