a0630f779455f25729e649c6162cbcbd9bacc56b
MichelLap <48298196+MichelLap@users.noreply.github.com>
2021-05-05 17:32:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 function setup() {
   let w = width;
-  let h = height
+  let h = height;
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(0, 100, 200);
-  text("width before Canvas: "+w+" / "+h,10,30);
-  text("width after Canvas: "+width+" / "+height,10,50);
-  text("displayWidth: "+displayWidth+" / "+displayHeight,10,70);
-  text("windowWidth: "+windowWidth+" / "+windowHeight,10,90);
+  fill(120);
+  rect(20,20,200,200);
+  fill(0);
+  text("width before Canvas: "+w+" / "+h,40,40);
+  text("width after Canvas: "+width+" / "+height,40,60);
+  text("displayWidth: "+displayWidth+" / "+displayHeight,40,80);
+  text("windowWidth: "+windowWidth+" / "+windowHeight,40,100);
   text("pixelDensity/displayDensity: "+pixelDensity()+" / "+displayDensity(),10,110);
 }
 
