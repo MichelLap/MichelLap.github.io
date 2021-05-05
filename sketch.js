46941dfce71d35f3910cf4d3df885a653ba3d370
MichelLap <48298196+MichelLap@users.noreply.github.com>
2021-05-05 19:17:52 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
+let w:
+let h:
+
 function setup() {
-  let w = width;
-  let h = height;
+  w = width;
+  h = height;
   createCanvas(windowWidth, windowHeight);
 }
 
@@ -8,8 +11,8 @@ function draw() {
   background(0, 100, 200);
   //fill(120);
   //rect(20,20,200,200);
-  stroke(0);
-  fill(0);
+  //stroke(0);
+  //fill(0);
   text("width before Canvas: "+w+" / "+h,40,40);
   text("width after Canvas: "+width+" / "+height,40,60);
   text("displayWidth: "+displayWidth+" / "+displayHeight,40,80);
